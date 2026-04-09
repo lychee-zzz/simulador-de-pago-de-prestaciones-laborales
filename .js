63bfda332768f3c074f422f82a2d97ba82b3c68d
Emily Chisaba <echisaba@unbosque.edu.co>
2026-04-09 08:43:09 -0500

    Declaración de variables iniciales en JS

---
+++ .js
@@ -0,0 +1,42 @@
+// Declaración de variables ingreso de información basica
+let NombreCompleto = "";
+let Edad = ;
+let TipodeDocumento = ;
+let NumerodeDocumento = "";
+
+
+//Validación de perfil
+if (Edad >= 18) 
+
+
+// Declaración de variables para el cálculo de salario
+let Salario = ;
+let Comisiones = ;
+let Totaldehorasextras = ;
+let ClasificaciondeNiveldeRiesgo = "";
+
+
+//Declaración de constantes para el cálculo de salario
+const IBC = 70% * ;
+const Salud = 4% * IBC;
+const Pension = 4% * IBC;
+const ARL = ;
+const Fondo_Solidaridad_Pensional = ;
+const Retencion_en_la_Fuente = ;
+const Edad_Minima = 18;
+const Edad_Maxima_del_Beneficio = 25;
+const Edad_Pension = 60;
+
+    //Valores de referencia para el año 2026
+        const Salario_Minimo_Legal_Vigente = 1.750.905;
+        const Salario_Minimo_Legal_Vigente = 22.761.765;
+        const Auxilio_Transporte = 249.095;
+        const Unidad_de_Valor_Tributario = 52.37;
+
+    //Tarifas de ARL (IBC)
+    const Riesgo_I_Mínimo = 0.522%;
+    const Riesgo_II_Bajo = 1.044%;
+    const Riesgo_III_Medio = 2.436%;
+    const Riesgo_IV_Alto = 4.350%;
+    const Riesgo_V_Máximo = 6.960%;
+
